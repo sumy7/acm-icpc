ebd15dd1a2464146fbcb7ad70d4d05d6d925246b
sumy <sunmingjian8@gmail.com>
2013-04-21 18:53:04 +0800

    Latin America 2012 Different Digits

---
+++ regional/latin-america-2012/D.cpp
@@ -0,0 +1,106 @@
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <map>
+#include <set>
+#include <bitset>
+#include <stack>
+#include <deque>
+#include <assert.h>
+
+using namespace std;
+#define BUG1 puts("BUG11\n")
+#define BUG2 puts("BUG22\n")
+#define rep(i,a,b) for(int (i)=(a);(i)<=(b);(i)++)
+#define rp(i,a) for(int i=0;i<a;i++)
+#define FD(i,a,b) for(int i=a;i>=b;i--)
+#define STOP  system("pause")
+#define PP printf(" ")
+#define mem(x,y) memset(x,y,sizeof(x))
+#define LN printf("\n");
+#define du freopen("in.txt","r",stdin)
+#define chu freopen("out.txt","w",stdout)
+#define EPS 1e-8
+#define FI first
+#define SE second
+#define PB push_back
+#define MP make_pair
+
+
+typedef long long LL;
+inline bool insize(int c,int l,int r){if (c>=l&&c<=r) return true; return false;}
+template<class T> inline void checkmin(T &a,T b){if(a == -1 || a > b)a = b;}
+template<class T> inline void checkmax(T &a,T b){if(a < b)    a = b;}
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int ,int> II;
+
+int dx[] = {0,1,0,-1};//up Right down Left
+int dy[] = {1,0,-1,0};
+template<class T> inline void sf(T& x)
+{
+    char c;
+    int mul = 1;
+    while((c = getchar()) != EOF)
+    {
+        if(c == '-')mul = -1;
+        if(c >= '0' && c <= '9')
+        {
+            x = c-'0';
+            break;
+        }
+    }
+    if(c == EOF){x = EOF;return;}
+    while((c = getchar()))
+    {
+        if(c >= '0' && c <= '9')
+        {
+            x = (x<<1)+(x<<3)+(c-'0');
+        }
+        else break;
+    }
+    x *= mul;
+}
+
+const int N=20000;       // 点数
+const int E=200555;   //边数
+const int INF= 0x3f3f3f3f;
+const long long  LINF= 0x3F3F3F3F3F3F3F3FLL;
+
+//
+
+int dp[8][1030];
+bool bo[20];
+VI dig;
+
+int solve(int x){
+    mem(bo,0);
+    for(;x;x/=10)
+        if(bo[x%10]==1)
+            return false;
+        else
+            bo[x%10]= 1;
+    return true;
+}
+
+int cal(int l, int r)
+{
+    int ans= 0;
+    rep(i,l,r)
+        if(solve(i))
+            ans++;
+    return ans;
+}
+
+int main(){
+    memset(dp,-1,sizeof(dp));
+    int L, R;
+    while(~scanf("%d%d", &L, &R)){
+        printf("%d\n", cal(L,R));
+    }
+
+}
